1c6d23baae33c9aa86aa195043934808f12f4218
conao3 <conao3@gmail.com>
2024-11-07 14:15:46 +0900

    first commit

---
+++ src/lsvisualizer/core.clj
@@ -0,0 +1,9 @@
+(ns lsvisualizer.core
+  (:require
+   [clojure.tools.logging :as log])
+  (:gen-class))
+
+(defn -main
+  "The entrypoint."
+  [& args]
+  (log/info args))
---
+++ test/lsvisualizer/core_test.clj
@@ -0,0 +1,6 @@
+(ns lsvisualizer.core-test
+  (:require
+   [clojure.test :as t]))
+
+(t/deftest sample-test
+  (t/is (= true true)))
